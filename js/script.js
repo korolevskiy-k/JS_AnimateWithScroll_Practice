98629fc6218fc25e8502c31424556ee7772d501d
KorolevskiyK <K-korolevskiy@mail.ru>
2022-06-25 18:58:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict"
+
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+    // Inizialization
+    function digitsCountersInit(digitsCountersItems) {
+        let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+        if (digitsCounters) {
+            digitsCounters.forEach(digitsCounter => {
+                digitsCountersAnimate(digitsCounter);
+            });
+        }
+    }
+    // Animation
+    function digitsCountersAnimate(digitsCounter) {
+        let startTimestamp = null;
+        const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 1000;
+        const startValue = parseInt(digitsCounter.innerHTML);
+        const startPosition = 0;
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    }
+    // digitsCountersInit();
+
+    // Start for scroll animation
+    let options = {
+        threshold: .3
+    }
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const targetElement = entry.target;
+                const digitsCountersItems = targetElement.querySelectorAll("[data-digits-counter]");
+                if(digitsCountersItems.length) {
+                    digitsCountersInit(digitsCountersItems);
+                }
+                observer.unobserve(targetElement);
+            }
+        });
+    }, options);
+
+    // Information
+    let section = document.querySelectorAll('.page__section');
+    if(section.length) {
+        section.forEach(section => {
+            observer.observe(section);
+        });
+    }
+}
